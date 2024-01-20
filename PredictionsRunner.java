6623f86d822488ee8932d060d752243e2cdfc5b2
Victor Vasquez <victordvasquez04@gmail.com>
2024-01-20 01:14:24 -0500

    Add list of teams and read it in PredictionsRunner

--- PredictionsRunner.java
+++ PredictionsRunner.java
@@ -1,8 +1,20 @@
+import java.util.ArrayList;
+
 public class PredictionsRunner {
 
-
     public static void main(String[] args) {
-        
+        ArrayList<Team> allTeams = new ArrayList<Team>();
+        readTeams(allTeams);
+    }
+
+    public static void readTeams(ArrayList<Team> teams) {
+        In in = new In("listofteams.txt");
+        while(in.hasNextLine()) {
+            String teamString = in.readLine();
+            String[] teamDetails = teamString.split(",");
+            Team newTeam = new Team(teamDetails[0], teamDetails[1], teamDetails[2]);
+            teams.add(newTeam);
+        }
     }
 }
 
